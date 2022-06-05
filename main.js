173149856e64a9afafe087c2036d1005a4bf2865
Emre Aslan <emreaslan7@icloud.com>
2022-06-05 11:31:03 +0300

    scroll top btn and finish

--- main.js
+++ main.js
@@ -2,7 +2,23 @@ const HamburgerMenu = document.querySelector("#navigation .nav-icon");
 const navContent = document.getElementById("nav-content");
 const closeBtn = document.querySelector('.close-button');
 const navitems = document.querySelectorAll('#nav-content .nav-item ul li a');
+const scrollbutton = document.querySelector(".scroll-top");
 
+/* Scroll button */
+if(scrollbutton){
+    window.addEventListener('scroll', ()=> {
+      if(scrollY > (window.innerHeight * 1.2)){
+        scrollbutton.style.display="flex";
+      }else{
+        scrollbutton.style.display="none";
+      }
+    });
+    scrollbutton.addEventListener("click", () => {
+      window.scrollTo(0, 0);
+    });
+  }
+
+/* Hamburger menu */  
 HamburgerMenu.addEventListener('click', ()=>{
     navContent.classList.add('show');
 })
